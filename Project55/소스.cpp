f7c60ce7c7f3c975354e68ae73db75c74015831b
이민지 <dbstnalswl123@naver.com>
2022-09-14 00:00:14 +0900

    초기 커밋.

---
+++ Project55/소스.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 1;
+	cin >> n;
+	cout << 1;
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			sum += i;
+			cout << " + " << i;
+		}
+	}
+	cout << " = " << sum << endl;
+
+	return 0;
+}
